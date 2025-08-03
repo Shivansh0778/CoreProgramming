89b8e0e91de471486225ad30bd923b13b2c50894
Shivansh0078 <shivanshsharma0778@gmail.com>
2025-08-03 21:31:43 +0530

    code for quetion 6 , L3

---
+++ Level3/FinalNumberChecker.java
@@ -0,0 +1,103 @@
+import java.util.*;
+
+public class FinalNumberChecker {
+
+    public static int[] findFactors(int number) {
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) count++;
+        }
+
+        int[] factors = new int[count];
+        int index = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) factors[index++] = i;
+        }
+        return factors;
+    }
+
+    public static int findGreatestFactor(int[] factors) {
+        int max = Integer.MIN_VALUE;
+        for (int f : factors) {
+            if (f > max) max = f;
+        }
+        return max;
+    }
+
+    public static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int f : factors) sum += f;
+        return sum;
+    }
+
+    public static int productOfFactors(int[] factors) {
+        int product = 1;
+        for (int f : factors) product *= f;
+        return product;
+    }
+
+    public static double productOfCubes(int[] factors) {
+        double product = 1;
+        for (int f : factors) product *= Math.pow(f, 3);
+        return product;
+    }
+
+    public static boolean isPerfect(int number) {
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) sum += i;
+        }
+        return sum == number;
+    }
+
+    public static boolean isAbundant(int number) {
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) sum += i;
+        }
+        return sum > number;
+    }
+
+    public static boolean isDeficient(int number) {
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) sum += i;
+        }
+        return sum < number;
+    }
+
+    public static boolean isStrong(int number) {
+        int sum = 0, temp = number;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += factorial(digit);
+            temp /= 10;
+        }
+        return sum == number;
+    }
+
+    public static int factorial(int n) {
+        int f = 1;
+        for (int i = 2; i <= n; i++) f *= i;
+        return f;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        int[] factors = findFactors(number);
+        System.out.println("Factors: " + Arrays.toString(factors));
+        System.out.println("Greatest Factor: " + findGreatestFactor(factors));
+        System.out.println("Sum of Factors: " + sumOfFactors(factors));
+        System.out.println("Product of Factors: " + productOfFactors(factors));
+        System.out.println("Product of Cubes of Factors: " + productOfCubes(factors));
+        System.out.println("Is Perfect Number: " + isPerfect(number));
+        System.out.println("Is Abundant Number: " + isAbundant(number));
+        System.out.println("Is Deficient Number: " + isDeficient(number));
+        System.out.println("Is Strong Number: " + isStrong(number));
+
+        sc.close();
+    }
+}
+
