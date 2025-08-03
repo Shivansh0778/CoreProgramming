dddeed9bca63b8812e14a6953a2b249c2fffde1a
Shivansh0078 <shivanshsharma0778@gmail.com>
2025-08-03 20:47:46 +0530

    code for quetion 7 , L2

---
+++ Level2/StudentVoteChecker.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class StudentVoteChecker {
+
+    public static boolean canStudentVote(int age) {
+        if (age < 0) {
+            return false;
+        }
+        return age >= 18;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] studentAges = new int[10];
+
+        for (int i = 0; i < studentAges.length; i++) {
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            studentAges[i] = scanner.nextInt();
+
+            boolean canVote = canStudentVote(studentAges[i]);
+
+            if (canVote) {
+                System.out.println("Student " + (i + 1) + " can vote.");
+            } else {
+                System.out.println("Student " + (i + 1) + " cannot vote.");
+            }
+        }
+    }
+}
+
