007cfe4e2743d08111e6ffb28056d4ee368674bb
Shivansh0078 <shivanshsharma0778@gmail.com>
2025-08-12 10:20:43 +0530

    code for question2 , L3

---
+++ Level3/UniqueCharacters.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class UniqueCharacters {
+
+    public static int getLength(String text) {
+        int count = 0;
+        try {
+            while (true) {
+                text.charAt(count);
+                count++;
+            }
+        } catch (Exception e) {
+        }
+        return count;
+    }
+
+    public static char[] findUniqueChars(String text) {
+        int len = getLength(text);
+        char[] temp = new char[len];
+        int count = 0;
+        for (int i = 0; i < len; i++) {
+            char ch = text.charAt(i);
+            boolean isUnique = true;
+            for (int j = 0; j < i; j++) {
+                if (text.charAt(j) == ch) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                temp[count] = ch;
+                count++;
+            }
+        }
+        char[] result = new char[count];
+        for (int i = 0; i < count; i++) {
+            result[i] = temp[i];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String text = sc.nextLine();
+        char[] uniqueChars = findUniqueChars(text);
+        System.out.print("Unique characters: ");
+        for (char c : uniqueChars) {
+            System.out.print(c + " ");
+        }
+        sc.close();
+    }
+}
