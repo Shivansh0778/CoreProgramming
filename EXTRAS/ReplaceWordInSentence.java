527248510ff0a5463244c638b1fb1ff71e8c72af
Shivansh0078 <shivanshsharma0778@gmail.com>
2025-08-13 11:08:24 +0530

    code for question12

---
+++ EXTRAS/ReplaceWordInSentence.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ReplaceWordInSentence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the sentence: ");
+        String sentence = sc.nextLine();
+        System.out.print("Enter the word to replace: ");
+        String oldWord = sc.next();
+        System.out.print("Enter the new word: ");
+        String newWord = sc.next();
+        
+        String updatedSentence = replaceWord(sentence, oldWord, newWord);
+        System.out.println("Updated sentence: " + updatedSentence);
+        
+        sc.close();
+    }
+    
+    public static String replaceWord(String sentence, String oldWord, String newWord) {
+        String[] words = sentence.split(" ");
+        String result = "";
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(oldWord)) {
+                result += newWord;
+            } else {
+                result += words[i];
+            }
+            if (i < words.length - 1) {
+                result += " ";
+            }
+        }
+        return result;
+    }
+}
+
