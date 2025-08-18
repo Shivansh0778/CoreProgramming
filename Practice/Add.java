fff17df5bf776799d2d415fb1ff237a628f30ecc
Shivansh0078 <shivanshsharma0778@gmail.com>
2025-08-18 10:32:32 +0530

    code for question2

---
+++ Practice/Add.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Add{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a: ");
+        int a = sc.nextInt();
+        System.err.println("enter b: ");
+        int b = sc.nextInt();
+        int c = a+b;
+        System.out.println("Sum of a+b :"+c);
+
+    }
+}
